f5bad56d7ef14af55ce4f38cd221ed02ccbd8a27
simonpod <s.podboraczynski@gmail.com>
2019-03-21 01:44:11 +0000

    test/menu visibility on click

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -50,22 +50,34 @@ $(function() {
 
 
           describe('The menu', function() {
+                 let bodyClass =  $('body').hasClass('menu-hidden');
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-            it('menu hidden', function() {
-                let bodyClass =  $('body').hasClass('menu-hidden');
+            it('menu default hidden', function() {
+
              expect(bodyClass).toBe(true);
         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+          * clicked and does it hide when clicked again.\
           */
+
+  it('menu changes visibility', function() {
+
+                $('.menu-icon-link').trigger('click');
+                expect($('body').hasClass('menu-hidden')).toBe(false);
+                $('.menu-icon-link').trigger('click');
+                expect($('body').hasClass('menu-hidden')).toBe(true);
+
+
+             });
+
 
 
       });
